0fba52fab1645c2f2328ad373e558df6a3b1253c
Sakshamsaks <saksham.agrawal.588@gmail.com>
2024-07-11 23:26:25 +0530

    Adding Sequelize into project.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express=require('express');
 const bodyParser=require('body-parser');
 
 const errorController=require('./controllers/error');
-const db=require('./util/database');
+const sequelize=require('./util/database');
 
 const app=express();
 
@@ -22,4 +22,11 @@ app.use(shopRouter);
 
 app.use(errorController.get404);
 
-app.listen(2000);
+sequelize.sync()
+.then(result=>{
+    app.listen(2000);
+})
+.catch(err=>{
+    console.log(err);
+})
+
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,26 +1,36 @@
 const Product=require('../models/product');
 const Cart=require('../models/cart');
+const { where } = require('sequelize');
 
 exports.getProducts=(req, res, next)=>{
-    Product.fetchAll()
-        .then(([rows, fieldData])=>{
+    Product.findAll()
+        .then(products=>{
             res.render('shop/product-list',{
-                prods:rows, 
+                prods:products, 
                 pageTitle: 'All Products', 
                 path:'/products', 
             });
         })
         .catch(err=>{
             console.log(err);
-        });;
+        });
 };
 
 exports.getProduct=(req,res,next)=>{
     const prodId=req.params.productId;
-    Product.findById(prodId)
-        .then(([product])=>{
+    /*Product.findAll({where:{id:prodId}})
+        .then(products=>{
             res.render('shop/product-detail',{
-                product:product[0],
+                product:products[0],
+                pageTitle:products[0].title,
+                path:'/products'
+            });
+        })
+        .catch(err=>console.log(err));*/
+    Product.findByPk(prodId)
+        .then(product=>{
+            res.render('shop/product-detail',{
+                product:product,
                 pageTitle:product.title,
                 path:'/products'
             });
@@ -31,10 +41,10 @@ exports.getProduct=(req,res,next)=>{
 };
 
 exports.getIndex=(req,res,next)=>{
-    Product.fetchAll()
-        .then(([rows, fieldData])=>{
+    Product.findAll()
+        .then(products=>{
             res.render('shop/index',{
-                prods:rows, 
+                prods:products, 
                 pageTitle: 'Shop', 
                 path:'/', 
             });
@@ -42,7 +52,6 @@ exports.getIndex=(req,res,next)=>{
         .catch(err=>{
             console.log(err);
         });
-    
 };
 
 exports.getCart=(req,res,next)=>{
