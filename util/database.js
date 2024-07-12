c0a281f53fb808515b54a7b4f6fe268e26499784
Sakshamsaks <saksham.agrawal.588@gmail.com>
2024-07-12 22:50:22 +0530

    Setup mongoDB database into our project

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize= require('sequelize');
+const mongodb=require('mongodb');
+const MongoClient=mongodb.MongoClient;
 
-const sequelize=new Sequelize('node-complete','root','saksham123',{
-    dialect:'mysql', 
-    host:'localhost'
-});
+const mongoConnect= (callback) => {
+    MongoClient.connect('mongodb+srv://saksham:W9Gqe1CXMq2WYEhf@cluster0.qcxjood.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+            .then(client=>{
+                console.log('Connected1');
+                callback(client);
+            })
+            .catch(err=>console.log(err));
+};
 
-module.exports=sequelize;
+module.exports=mongoConnect;
