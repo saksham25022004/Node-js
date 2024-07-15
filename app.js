20b8122a4445cfad909d0bc02603238df5d34067
Sakshamsaks <saksham.agrawal.588@gmail.com>
2024-07-16 00:33:18 +0530

    Authentication implement and CSRF implement

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser=require('body-parser');
 const mongoose = require('mongoose');
 const session=require('express-session');
 const MongoDBStore=require('connect-mongodb-session')(session);
+const csrf=require('csurf');
 
 const errorController=require('./controllers/error');
 const User= require('./models/user');
@@ -16,6 +17,8 @@ const store=new MongoDBStore({
     uri: MONGODB_URI,
     collection:'sessions'
 });
+
+const csrfProtection=csrf();
 
 app.set('view engine','ejs');
 app.set('views', 'views');
@@ -33,6 +36,7 @@ app.use(session({
       saveUninitialized:false,
       store:store
     }))
+app.use(csrfProtection);
 
 app.use((req,res,next)=>{
     User.findById('66940c502606db1d4494096d')
@@ -55,6 +59,12 @@ app.use((req,res,next)=>{
         .catch(err=>console.log(err))
 })
 
+app.use((req,res,next)=>{
+    res.locals.isAuthenticated=req.session.isLoggedIn;
+    res.locals.csrfToken=req.csrfToken();
+    next();
+})
+
 app.use('/admin',adminData);
 app.use(shopRouter);
 app.use(authRouter);
@@ -63,18 +73,6 @@ app.use(errorController.get404);
 
 mongoose.connect(MONGODB_URI)
     .then(result=>{
-        User.findOne().then(user=>{
-            if(!user){
-                const user=new User({
-                    name:'Saksham',
-                    email:'saksham@gmail.com',
-                    cart:{
-                        items:[]
-                    }
-                });
-                user.save();
-            }
-        });
         app.listen(2000);
     })
     .catch(err=>{
