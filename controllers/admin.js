56273901a41c85aec4f70487f831444c72befe29
Sakshamsaks <saksham.agrawal.588@gmail.com>
2024-07-18 16:42:41 +0530

    PDF Generate, Upload and Delete in node sever using PDFKit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,7 @@
 const { ValidationError } = require('sequelize');
 const Product=require('../models/product');
 const {validationResult}=require('express-validator');
+const fileHelper=require('../util/file');
 
 exports.getAddProduct=(req, res, next)=>{
     res.render('admin/edit-product',{
@@ -125,6 +126,7 @@ exports.postEditProduct=(req,res,next)=>{
             product.price=updatedPrice;
             product.description=updatedDesc;
             if(image){
+                fileHelper.deleteFile(product.imageUrl);
                 product.imageUrl=image.path;
             }
             return product.save().then(result=>{
@@ -156,7 +158,14 @@ exports.getProducts=(req,res,next)=>{
 
 exports.postDeleteProduct=(req,res,next)=>{
     const prodId=req.body.productId;
-    Product.deleteOne({_id:prodId, userId: req.user._id})
+    Product.findById(prodId)
+        .then(product=>{
+            if(!product){
+                return next(new Error('Product not found.'));
+            }
+            fileHelper.deleteFile(product.imageUrl);
+            return Product.deleteOne({_id:prodId, userId: req.user._id});
+        })
         .then(result=>{
             res.redirect('/admin/products');
         })
