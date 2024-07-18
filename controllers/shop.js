a9a2793a07ae5da9131b31ae7a625f58fa757028
Sakshamsaks <saksham.agrawal.588@gmail.com>
2024-07-18 19:14:44 +0530

    Implementing Pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,14 +5,30 @@ const Order=require('../models/order');
 const { application } = require('express');
 const PDFDocument=require('pdfkit');
 
+const ITEMS_PER_PAGE=1;
+
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page=+req.query.page || 1;
+  let totalItems;
+
+  Product.find().countDocuments()
+    .then(numProducts=>{
+      totalItems=numProducts;
+      return Product.find()
+        .skip((page-1)*ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
+        pageTitle: 'products',
         path: '/products',
-        isAuthenticated: req.session.isLoggedIn
+        currentPage:page,
+        hasNextPage:ITEMS_PER_PAGE*page<totalItems,
+        hasPrevPage:page>1,
+        nextPage:page+1,
+        prevPage:page-1,
+        lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE)
       });
     })
     .catch(err=>{
@@ -41,12 +57,27 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page=+req.query.page || 1;
+  let totalItems;
+
+  Product.find().countDocuments()
+    .then(numProducts=>{
+      totalItems=numProducts;
+      return Product.find()
+        .skip((page-1)*ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        currentPage:page,
+        hasNextPage:ITEMS_PER_PAGE*page<totalItems,
+        hasPrevPage:page>1,
+        nextPage:page+1,
+        prevPage:page-1,
+        lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE)
       });
     })
     .catch(err=>{
